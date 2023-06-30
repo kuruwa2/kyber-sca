09882e7034d52dff8310b22eee927fd098980bf9
Kuruwa <a93019050612@gmail.com>
2023-06-30 13:24:47 +0900

    Add files via upload

---
+++ gentraces.py
@@ -0,0 +1,30 @@
+import json
+from scipy.signal import decimate
+
+data = bytes.fromhex(''.join(open("./ELMO/data.txt", 'r').read().split()))
+
+#sk = data[:1632]
+#data = data[2432:]
+sk = data[:3168]
+data = data[4736:]
+
+traces = {}
+traces['sk'] = list(sk)
+
+sess = []
+for i in range(800):
+    ses = {}
+    ses['ct'] = list(data[:768])
+    data = data[768:]
+    pm = []
+    with open(f"./ELMO/output/traces/trace{str(i+1).rjust(5,'0')}.trc", 'r') as f:
+        for i in f:
+            pm.append(float(i))
+    pm = list(decimate(pm, 5))
+    ses['pm'] = pm
+    sess.append(ses)
+
+traces["sessions"] = sess
+f = open("./traces1024.json", 'w')
+f.write(json.dumps(traces))
+f.close()
